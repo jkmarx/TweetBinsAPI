19e70113819b8a1eb38aa1a96b081dd8a591efc6
J.Marx <jennyrutledge@gmail.com>
2015-04-19 16:11:47 -0400

    Update production rb file.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   belongs_to :user
   has_many :friends, dependent: :destroy
 
-  validates_uniqueness_of :friends, scope: :twitterId
+  validates_uniqueness_of :friends, scope: :twitterIdgit
 end
